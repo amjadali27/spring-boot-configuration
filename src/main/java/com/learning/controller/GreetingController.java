181e3d6970828c94aec29511e9be6a5505208896
Amjad Ali <ali.amjad@systemsltd.com>
2021-10-01 23:27:57 +0500

    Used @Value with static, default, list and keyvalue pairs

--- src/main/java/com/learning/controller/GreetingController.java
+++ src/main/java/com/learning/controller/GreetingController.java
@@ -4,15 +4,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class GreetingController {
 
-    @Value("${greeting.message}")
+    @Value("${greeting.message:default message}")
     private String greetingMessage;
+
+    @Value("My static message")
+    private String staticMessage;
+
+    @Value("${mylist}")
+    private List<String> myList;
+
+    @Value("#{${dbValues}}")
+    private Map<String,String> dbValues;
 
     @RequestMapping("greeting")
     public String greeting(){
-        return greetingMessage;
+        return greetingMessage+"\n"+staticMessage+"  My List:: "+myList;
     }
-
 }
