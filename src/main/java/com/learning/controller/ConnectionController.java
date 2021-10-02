bfa94805049c031c5ee094639767ea76a8d85d83
Amjad Ali <ali.amjad@systemsltd.com>
2021-10-02 15:12:27 +0500

    Added env-details end point in which Environment object is used to get properties

--- src/main/java/com/learning/controller/ConnectionController.java
+++ src/main/java/com/learning/controller/ConnectionController.java
@@ -2,6 +2,7 @@ package com.learning.controller;
 
 import com.learning.bean.DbConnection;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,16 @@ public class ConnectionController {
     @Autowired
     private DbConnection dbConnection;
 
+    @Autowired
+    private Environment env;
+
     @RequestMapping("db-connection")
     public String getDbConnectionDetails(){
         return "URL: "+dbConnection.getConnectionUrl()+"----HOST: "+dbConnection.getHost()+"----PORT: "+dbConnection.getPort();
+    }
+
+    @RequestMapping("/env-details")
+    public String getEnvDetails(){
+        return env.toString();
     }
 }
